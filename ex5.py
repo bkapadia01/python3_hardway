8fa00b4da07d6a5f908285789408ed7ed0f6d39c
Bhavin <bkapadia@figure1.com>
2019-06-25 18:20:17 -0400

    ex5

---
+++ ex5.py
@@ -0,0 +1,13 @@
+my_name = 'bkap'
+my_age = 2
+my_height = 33 #test
+my_weight = 100
+my_eyes = 'blue'
+my_hair = 'black'
+
+print(f"my name is {my_name}")
+print(f"i am {my_age} years old")
+print(f"my eyes are {my_eyes} and my hair is {my_hair}")
+
+total = my_age + my_height + my_weight
+print(f"the total of my age, height and weight  is {total}")
