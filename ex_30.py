14a1d22249ede9e495bbc34355c3dd336bfd310c
Bhavin <bkapadia@figure1.com>
2019-07-11 17:35:02 -0400

    ex_30

---
+++ ex_30.py
@@ -0,0 +1,22 @@
+people = 30
+cars = 40
+trucks = 15
+
+if cars > people:
+    print("take the car")
+elif cars < people:
+    print("dont take the cars")
+else:
+    print("we cant decide")
+
+if trucks > cars:
+    print("take the truck")
+elif trucks < cars:
+    print("maybe take the trucks")
+else:
+    print("we cant decide")
+
+if people > trucks:
+    print("take the truckss")
+else:
+    print("lets stay home")
